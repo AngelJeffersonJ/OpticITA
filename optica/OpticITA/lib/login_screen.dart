e2b42fa5a7cdfc0de586206302ba4ec5969bc970
AngelJeffersonJ <jeffersonreincarnation@hotmail.com>
2024-04-24 17:26:12 -0600

    login

---
+++ optica/OpticITA/lib/login_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State
--- optica/OpticITA/lib/main.dart
+++ optica/OpticITA/lib/main.dart
@@ -39,6 +39,15 @@ class _CatalogoLentesState extends State<CatalogoLentes> {
       appBar: AppBar(
         title: Text('Catálogo de Lentes'),
         actions: [
+          IconButton(
+            icon: Icon(Icons.login),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
+          ),
           PopupMenuButton<int>(
             onSelected: (value) {
               setState(() {
@@ -253,6 +262,50 @@ class DetalleProducto extends StatelessWidget {
               child: Text('Agregar al Carrito'),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Iniciar Sesión'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Correo Electrónico',
+                  prefixIcon: Icon(Icons.email),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Implementar lógica de autenticación
+                  Navigator.pop(context); // Simula un inicio de sesión exitoso
+                },
+                child: Text('Iniciar Sesión'),
+              ),
+            ],
+          ),
         ),
       ),
     );
