1281aff535f1f942f3ea27c071cd407e0a12b90f
karenvh01 <kvianeyhdzhermosillo17@gmail.com>
2024-04-28 17:50:27 -0600

    se agregó un registro y más productos al catálogo

--- optica/OpticITA/lib/main.dart
+++ optica/OpticITA/lib/main.dart
@@ -48,6 +48,17 @@ class _CatalogoLentesState extends State<CatalogoLentes> {
               );
             },
           ),
+
+          IconButton(
+            icon: Icon(Icons.person_add),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => RegisterScreen()),
+              );
+            },
+          ),
+    
           PopupMenuButton<int>(
             onSelected: (value) {
               setState(() {
@@ -73,15 +84,48 @@ class _CatalogoLentesState extends State<CatalogoLentes> {
         child: ListView(
           children: [
             ProductoCard(
-              nombre: 'Lentes de Sol Ray-Ban',
-              imagenPath: 'assets/images/rayban.png',
-              precio: 150,
+              nombre: 'Lente Oftálmico Gucci GG0027O Havana',
+              imagenPath: 'assets/images/L1-1.png',
+              description: 'Los Lentes oftálmicos Gucci GG0027O para mujer cuentan con un diseño marco completo redondo que les da un estilo muy femenino y atractivo, para que expreses tu personalidad. Fabricado con acetato color havana super resistentes y cómodos de usar para altas jornadas del día. Recomendado para rostros cuadrados y forma de diamante. Sé parte de este modelo y complementa tu look con Gucci.',
+              precio: 800,
             ),
             ProductoCard(
-              nombre: 'Lentes de Lectura',
-              imagenPath: 'assets/images/lectura.png',
-              precio: 80,
+              nombre: 'Lente Oftálmico Martha Debayle JOAN Negro',
+              imagenPath: 'assets/images/L2-1.png',
+              description: 'Los lentes oftálmicos JOAN son inspirados en Joan Crawford tienen un marco completo en forma cuadrada oversize fabricado En acetato negro. Las varillas están hechas de metal en color dorado con diseño circular garigoleado. Sus terminales son de rayas en blanco y negro. Recomendado para rostros ovalados y forma de diamante.',
+              precio: 575,
             ),
+            ProductoCard(
+              nombre: 'Lente Oftálmico Ray Ban RB7047O Negro',
+              imagenPath: 'assets/images/L3-1.png',
+              description: 'Los Lentes Oftálmicos Ray-Ban RB7047O cuentan con un diseño rectangular que les da un estilo clásico para que expreses tu personalidad. Fabricado en acetato, este armazón es muy cómodo, resistente y duradero. El armazón Ray-Ban oftálmico viene en color negro mate que le da un toque de modernidad a lo clásico.',
+              precio: 900,
+            ),
+            ProductoCard(
+              nombre: 'Lentes de Contacto Lunare cosmético Neutro',
+              imagenPath: 'assets/images/L4-1.png',
+              description: 'Los lentes de contacto Lunare están fabricados con la tecnología Tri-Kolor, que gracias a sus tres capas de color ofrece un aspecto más natural a tus ojos y la mayor comodidad.',
+              precio: 500,
+            ),
+            ProductoCard(
+              nombre: 'Lentes de Sol Ray-Ban JA-JO',
+              imagenPath: 'assets/images/L5-1.png',
+              description: 'Usa la energía de los ojos brillantes y el ambiente jovial del mundo de los festivales con el nuevo estilo esencial veraniego - que trae el JA-JO. Hemos añadido más impacto a las atemporales gafas redondas de metal con una explosión de color para lucir un estilo elegante y contemporáneo.',
+              precio: 420,
+            ),
+            ProductoCard(
+              nombre: 'Lentes de lectura',
+              imagenPath: 'assets/images/lectura.png',
+              description: 'Usa la energía de los ojos brillantes y el ambiente jovial del mundo de los festivales con el nuevo estilo esencial veraniego - que trae el JA-JO. Hemos añadido más impacto a las atemporales gafas redondas de metal con una explosión de color para lucir un estilo elegante y contemporáneo.',
+              precio: 420,
+            ),
+            ProductoCard(
+              nombre: 'Lentes Rayban',
+              imagenPath: 'assets/images/Rayban.png',
+              description: 'Usa la energía de los ojos brillantes y el ambiente jovial del mundo de los festivales con el nuevo estilo esencial veraniego - que trae el JA-JO. Hemos añadido más impacto a las atemporales gafas redondas de metal con una explosión de color para lucir un estilo elegante y contemporáneo.',
+              precio: 420,
+            ),
+
           ],
         ),
       ),
@@ -166,12 +210,14 @@ class _CatalogoLentesState extends State<CatalogoLentes> {
 class ProductoCard extends StatelessWidget {
   final String nombre;
   final String imagenPath;
+  final String description;
   final double precio;
 
   const ProductoCard({
     Key? key,
     required this.nombre,
     required this.imagenPath,
+    required this.description,
     required this.precio,
   }) : super(key: key);
 
@@ -196,6 +242,7 @@ class ProductoCard extends StatelessWidget {
                   builder: (context) => DetalleProducto(
                     nombre: nombre,
                     imagenPath: imagenPath,
+                    description: description,
                     precio: precio,
                   ),
                 ),
@@ -211,12 +258,14 @@ class ProductoCard extends StatelessWidget {
 class DetalleProducto extends StatelessWidget {
   final String nombre;
   final String imagenPath;
+  final String description;
   final double precio;
 
   const DetalleProducto({
     Key? key,
     required this.nombre,
     required this.imagenPath,
+    required this.description,
     required this.precio,
   }) : super(key: key);
 
@@ -226,16 +275,19 @@ class DetalleProducto extends StatelessWidget {
       appBar: AppBar(
         title: Text('Detalle del Producto'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // Imagen
             Image.asset(
               imagenPath,
               fit: BoxFit.cover,
-              height: 150,
+              height: 150, // Ajusta esta altura según tu preferencia
             ),
             SizedBox(height: 20),
+            // Nombre del producto
             Text(
               nombre,
               style: TextStyle(
@@ -244,6 +296,16 @@ class DetalleProducto extends StatelessWidget {
               ),
             ),
             SizedBox(height: 10),
+            // Descripción
+            Text(
+              description,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            // Precio
             Text(
               '\$$precio',
               style: TextStyle(
@@ -252,6 +314,7 @@ class DetalleProducto extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
+            // Botón
             ElevatedButton(
               onPressed: () {
                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -311,3 +374,73 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
+
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registrarse'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Nombre de usuario',
+                  icon: Icon(Icons.person),
+                ),
+                keyboardType: TextInputType.text,
+              ),
+              SizedBox(height: 20),
+              
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Correo Electrónico',
+                  prefixIcon: Icon(Icons.email),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Teléfono',
+                  prefixIcon: Icon(Icons.phone),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Confirmar Contraseña',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Implementar lógica de registro
+                  Navigator.pop(context); // Simula un registro exitoso
+                },
+                child: Text('Registrarse'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ optica/OpticITA/lib/register.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State
